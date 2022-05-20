5d09aaf24e1f118d242f38a700c4d3360d3036e5
Janine <janine.rugayan@ntnu.no>
2022-05-21 00:00:14 +0200

    moved .cpu()

--- fine_tuning_wav2vec.py
+++ fine_tuning_wav2vec.py
@@ -193,12 +193,12 @@ wer_metric = load_metric("wer")
 
 
 def compute_metrics(pred):
-    pred_logits = pred.predictions.cpu()
+    pred_logits = pred.predictions
     pred_ids = np.argmax(pred_logits, axis=-1)
 
     pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
 
-    pred_str = processor.batch_decode(pred_ids)  # this causing failure in evaluation??
+    pred_str = processor.batch_decode(pred_ids).cpu()  # this causing failure in evaluation??
 
     # we do not want to group tokens when computing the metrics
     label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
