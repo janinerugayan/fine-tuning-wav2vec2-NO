05b3493d8a47644f2705c1afad5b9559674eafa9
Janine <janine.rugayan@ntnu.no>
2022-06-27 16:47:56 +0200

    just Rundkast data

--- fine_tuning_wav2vec2_v3.py
+++ fine_tuning_wav2vec2_v3.py
@@ -151,8 +151,7 @@ print("Loading dataset direct from data dir to pandas dataframe")
 #                  "../../datasets/NordTrans_TUL/train/NRK/",
 #                  "../../datasets/NordTrans_TUL/train/Rundkast_cuts_random25/"]
 
-data_dir_list = ["../../datasets/NordTrans_TUL/train/NRK/",
-                 "../../datasets/NordTrans_TUL/train/Rundkast_cuts_random25/"]
+data_dir_list = ["../../datasets/NordTrans_TUL/train/Rundkast_cuts_random25/"]
 
 csv_export_dir = "../../model_ckpts/fine-tuning_wav2vec2_v3/runs/"
 
