39c5a54ff5ba18c35791863d5d5be881c2b42998
Janine <janine.rugayan@ntnu.no>
2022-07-13 11:50:25 +0200

    changed model and learning rate, v12: 300m

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -6,11 +6,11 @@
 # mkdir wav2vec2_NO_v12
 # cd ../github/fine-tuning-wav2vec2-NO/
 
-python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
+python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
                                        --fine_tuned_model_ver="fine-tuning_wav2vec2_v12"\
                                        --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v12/"\
                                        --num_train_epochs=30\
-                                       --learning_rate=2e-5
+                                       --learning_rate=1e-4
 
 # python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
 #                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v9"\
