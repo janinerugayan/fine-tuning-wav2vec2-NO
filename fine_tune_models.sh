b8071b1715dc946f9c7fe48256e4c2e480dbe7c8
Janine <janine.rugayan@ntnu.no>
2022-07-15 14:06:43 +0200

    v14: 5 epochs, all train data

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -1,15 +1,15 @@
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_v13
-cd fine-tuning_wav2vec2_v13
+mkdir fine-tuning_wav2vec2_v14
+cd fine-tuning_wav2vec2_v14
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_v13
+mkdir wav2vec2_NO_v14
 cd ../github/fine-tuning-wav2vec2-NO/
 
 python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v13"\
-                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v13/"\
-                                       --num_train_epochs=12\
+                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v14"\
+                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v14/"\
+                                       --num_train_epochs=5\
                                        --learning_rate=1e-4
 
 # cd ../model_ckpts/
