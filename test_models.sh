6d08da7aa2d265aa7a2e341d27b7ccdf05153459
Janine <janine.rugayan@ntnu.no>
2022-07-16 13:42:37 +0200

    test v14

--- test_models.sh
+++ test_models.sh
@@ -1,6 +1,6 @@
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v13/"\
-                        --log_file="./logs/test_log_wav2vec2_v13.txt"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v14/"\
+                        --log_file="./logs/test_log_wav2vec2_v14.txt"\
                         --get_orig_model_results=0
 
 # python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
