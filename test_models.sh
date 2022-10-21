da8092ec2393ac294fab557364ff5ce7173f3a30
Janine <janine.rugayan@ntnu.no>
2022-10-21 23:43:19 +0200

    test v15 checkpoint 27500

--- test_models.sh
+++ test_models.sh
@@ -1,7 +1,12 @@
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v12/"\
-                        --log_file="./logs/test_log_wav2vec2_v12.txt"\
+                        --fine_tuned_model="../../model_ckpts/fine-tuning_wav2vec2_v15_extracted/checkpoint-27500_extracted/"\
+                        --log_file="./logs/test_log_wav2vec2_v15_checkpoint-27500_extracted.txt"\
                         --get_orig_model_results=0
+
+# python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+#                         --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v15/"\
+#                         --log_file="./logs/test_log_wav2vec2_v15.txt"\
+#                         --get_orig_model_results=0
 
 # python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
 #                         --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v9/"\
