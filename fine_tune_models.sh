214614c88b27967b002257760c217024ab83d652
Janine <janine.rugayan@ntnu.no>
2022-10-22 10:33:42 +0200

    v16 continue fine-tuning to epoch 12

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -1,15 +1,15 @@
-# cd ../../model_ckpts/
-# mkdir fine-tuning_wav2vec2_v15
-# cd fine-tuning_wav2vec2_v15
-# mkdir runs
-# cd ../../fine_tuned_models/
-# mkdir wav2vec2_NO_v15
-# cd ../github/fine-tuning-wav2vec2-NO/
+cd ../../model_ckpts/
+mkdir fine-tuning_wav2vec2_v16
+cd fine-tuning_wav2vec2_v16
+mkdir runs
+cd ../../fine_tuned_models/
+mkdir wav2vec2_NO_v16
+cd ../github/fine-tuning-wav2vec2-NO/
 
 python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v15"\
-                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v15/"\
-                                       --num_train_epochs=6\
+                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v16"\
+                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v16/"\
+                                       --num_train_epochs=12\
                                        --learning_rate=1e-4
 
 # cd ../model_ckpts/
@@ -22,6 +22,6 @@ python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-30
 #
 # python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
 #                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v15"\
-#                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v15/"\
+#                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v16/"\
 #                                       --num_train_epochs=12\
 #                                       --learning_rate=2e-5
