b6ea3054282f88177efe3a70606098d3e430def0
Janine <janine.rugayan@ntnu.no>
2022-11-09 10:24:21 +0100

    v17: fine tuning 1B param model with asd metric

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -1,27 +1,20 @@
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_v16
-cd fine-tuning_wav2vec2_v16
+mkdir fine-tuning_wav2vec2_v17
+cd fine-tuning_wav2vec2_v17
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_v16
+mkdir wav2vec2_NO_v17
 cd ../github/fine-tuning-wav2vec2-NO/
 
-python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v16"\
-                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v16/"\
-                                       --num_train_epochs=12\
-                                       --learning_rate=1e-4
+# python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+#                                        --fine_tuned_model_ver="fine-tuning_wav2vec2_v16"\
+#                                        --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v16/"\
+#                                        --num_train_epochs=12\
+#                                        --learning_rate=1e-4
 
-# cd ../model_ckpts/
-# mkdir fine-tuning_wav2vec2_v15
-# cd fine-tuning_wav2vec2_v15
-# mkdir runs
-# cd ../../fine_tuned_models/
-# mkdir wav2vec2_NO_v15
-# cd ../github/fine-tuning-wav2vec2-NO/
-#
-# python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
-#                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v15"\
-#                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v16/"\
-#                                       --num_train_epochs=12\
-#                                       --learning_rate=2e-5
+
+python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
+                                      --fine_tuned_model_ver="fine-tuning_wav2vec2_v17"\
+                                      --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v17/"\
+                                      --num_train_epochs=12\
+                                      --learning_rate=2e-5
