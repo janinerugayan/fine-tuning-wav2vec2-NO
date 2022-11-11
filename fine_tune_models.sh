f3e84a6b48249f369948616e0b755e798188bac1
Janine <janine.rugayan@ntnu.no>
2022-11-11 20:58:27 +0100

    v17: train small, less hours but equal proportion of all data

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -6,15 +6,15 @@ cd ../../fine_tuned_models/
 mkdir wav2vec2_NO_v17
 cd ../github/fine-tuning-wav2vec2-NO/
 
-# python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-#                                        --fine_tuned_model_ver="fine-tuning_wav2vec2_v16"\
-#                                        --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v16/"\
-#                                        --num_train_epochs=12\
-#                                        --learning_rate=1e-4
+python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v17"\
+                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v17/"\
+                                       --num_train_epochs=12\
+                                       --learning_rate=1e-4
 
 
-python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
-                                      --fine_tuned_model_ver="fine-tuning_wav2vec2_v17"\
-                                      --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v17/"\
-                                      --num_train_epochs=12\
-                                      --learning_rate=2e-5
+# python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
+#                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v17"\
+#                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v17/"\
+#                                       --num_train_epochs=12\
+#                                       --learning_rate=2e-5
