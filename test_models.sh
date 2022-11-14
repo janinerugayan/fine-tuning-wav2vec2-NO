a715cd907689ec4de21c8a59fc61c493feaddbcc
Janine <janine.rugayan@ntnu.no>
2022-11-14 15:39:48 +0100

    test script for model v18

--- test_models.sh
+++ test_models.sh
@@ -3,4 +3,11 @@ python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
                         --log_file="./logs/test_log_wav2vec2_v18_ASDmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="asd_metric.py"\
-                        --extract_transcriptions=1\
+                        --extract_transcriptions=1
+
+python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v18/"\
+                        --log_file="./logs/test_log_wav2vec2_v18_WERmetric.txt"\
+                        --get_orig_model_results=0\
+                        --metric_to_use="wer"\
+                        --extract_transcriptions=0
