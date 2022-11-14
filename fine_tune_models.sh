3876f69d724b639aae21d45da7caf9aedf02e62d
Janine <janine.rugayan@ntnu.no>
2022-11-14 13:55:06 +0100

    changed number of epochs to just 3

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -9,7 +9,7 @@ cd ../github/fine-tuning-wav2vec2-NO/
 python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
                                        --fine_tuned_model_ver="fine-tuning_wav2vec2_v18"\
                                        --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v18/"\
-                                       --num_train_epochs=12\
+                                       --num_train_epochs=3\
                                        --learning_rate=1e-4\
                                        --use_asd_metric=0
 
@@ -24,9 +24,9 @@ cd ../github/fine-tuning-wav2vec2-NO/
 python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
                                        --fine_tuned_model_ver="fine-tuning_wav2vec2_v19"\
                                        --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v19/"\
-                                       --num_train_epochs=12\
+                                       --num_train_epochs=3\
                                        --learning_rate=1e-4\
-                                       --use_asd_metric=1                        
+                                       --use_asd_metric=1
 
 
 # python fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-1b-bokmaal"\
