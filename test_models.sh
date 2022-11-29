e4ae56adadaf65cf274fcb069655290189672141
Janine <janine.rugayan@ntnu.no>
2022-11-29 10:12:32 +0100

    test bash script for v20 & v21

--- test_models.sh
+++ test_models.sh
@@ -1,27 +1,27 @@
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v18/"\
-                        --log_file="./logs/test_log_wav2vec2_v18_ASDmetric.txt"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v20/"\
+                        --log_file="./logs/test_log_wav2vec2_v20_ASDmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="asd_metric.py"\
                         --extract_transcriptions=1
 
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v18/"\
-                        --log_file="./logs/test_log_wav2vec2_v18_WERmetric.txt"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v20/"\
+                        --log_file="./logs/test_log_wav2vec2_v20_WERmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="wer"\
                         --extract_transcriptions=0
 
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v19/"\
-                        --log_file="./logs/test_log_wav2vec2_v19_ASDmetric.txt"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v21/"\
+                        --log_file="./logs/test_log_wav2vec2_v21_ASDmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="asd_metric.py"\
                         --extract_transcriptions=1
 
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v19/"\
-                        --log_file="./logs/test_log_wav2vec2_v19_WERmetric.txt"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_v21/"\
+                        --log_file="./logs/test_log_wav2vec2_v21_WERmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="wer"\
                         --extract_transcriptions=0
