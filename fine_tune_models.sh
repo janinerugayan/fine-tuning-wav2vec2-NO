ddee3ef3fdac4a6710dc166c19ed3d5103660c72
Janine <janine.rugayan@ntnu.no>
2022-12-01 13:05:25 +0100

    fine-tuning to 12 epochs (orig loss)

--- fine_tune_models.sh
+++ fine_tune_models.sh
@@ -1,30 +1,30 @@
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_v20
-cd fine-tuning_wav2vec2_v20
+mkdir fine-tuning_wav2vec2_v22
+cd fine-tuning_wav2vec2_v22
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_v20
+mkdir wav2vec2_NO_v22
 cd ../github/fine-tuning-wav2vec2-NO/
 
 python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v20"\
-                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v20/"\
-                                       --num_train_epochs=6\
+                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v22"\
+                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v22/"\
+                                       --num_train_epochs=12\
                                        --learning_rate=1e-4\
                                        --use_asd_metric=0
 
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_v21
-cd fine-tuning_wav2vec2_v21
+mkdir fine-tuning_wav2vec2_v23
+cd fine-tuning_wav2vec2_v23
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_v21
+mkdir wav2vec2_NO_v23
 cd ../github/fine-tuning-wav2vec2-NO/
 
 python3 fine_tuning_wav2vec2_working.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v21"\
-                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v21/"\
-                                       --num_train_epochs=6\
+                                       --fine_tuned_model_ver="fine-tuning_wav2vec2_v23"\
+                                       --export_model_dir="../../fine_tuned_models/wav2vec2_NO_v23/"\
+                                       --num_train_epochs=12\
                                        --learning_rate=1e-4\
                                        --use_asd_metric=1
 
