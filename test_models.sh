9cd0281087ba68e82d90744324dc1f3b8aed9a6f
Janine <janine.rugayan@ntnu.no>
2022-12-01 13:03:18 +0100

    test script for custom loss v1

--- test_models.sh
+++ test_models.sh
@@ -1,12 +1,14 @@
+# fine-tuned model path should be withouth "/" at the end of the path
+
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_customLossV1/"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_customLossV1"\
                         --log_file="./logs/test_log_wav2vec2_NO_customLossV1_ASDmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="asd_metric.py"\
                         --extract_transcriptions=1
 
 python test_wav2vec2.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_customLossV1/"\
+                        --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_customLossV1"\
                         --log_file="./logs/test_log_wav2vec2_NO_customLossV1_WERmetric.txt"\
                         --get_orig_model_results=0\
                         --metric_to_use="wer"\
