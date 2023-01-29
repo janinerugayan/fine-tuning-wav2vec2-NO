c3b251b16c219568aba7bdfd4bab362fd3397fc0
Janine <janine.rugayan@ntnu.no>
2023-01-29 23:14:32 +0100

    v3 finetuning without using asd metric

--- fine_tune_models_customloss.sh
+++ fine_tune_models_customloss.sh
@@ -1,14 +1,14 @@
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_customLossV2
-cd fine-tuning_wav2vec2_customLossV2
+mkdir fine-tuning_wav2vec2_customLossV3
+cd fine-tuning_wav2vec2_customLossV3
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_customLossV2
+mkdir wav2vec2_NO_customLossV3
 cd ../github/fine-tuning-wav2vec2-NO/
 
 python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV2"\
-                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV2/"\
+                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV3"\
+                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV3/"\
                                            --num_train_epochs=3\
                                            --learning_rate=1e-4\
-                                           --use_asd_metric=1
+                                           --use_asd_metric=0
