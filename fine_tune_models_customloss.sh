407c37b11f5d39d402eb6ba6415fdfa7b3af00de
Janine <janine.rugayan@ntnu.no>
2023-03-20 15:12:13 +0100

    redoing custom loss experiments

--- fine_tune_models_customloss.sh
+++ fine_tune_models_customloss.sh
@@ -1,14 +1,31 @@
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_customLossV3
-cd fine-tuning_wav2vec2_customLossV3
+mkdir fine-tuning_wav2vec2_origLossV1
+cd fine-tuning_wav2vec2_origLossV1
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_customLossV3
+mkdir wav2vec2_NO_origLossV1
 cd ../github/fine-tuning-wav2vec2-NO/
 
-python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV3"\
-                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV3/"\
+python3 fine_tuning_wav2vec2_origLoss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_origLossV1"\
+                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_origLossV1/"\
                                            --num_train_epochs=3\
                                            --learning_rate=1e-4\
                                            --use_asd_metric=0
+
+
+
+cd ../../model_ckpts/
+mkdir fine-tuning_wav2vec2_customLossV1
+cd fine-tuning_wav2vec2_customLossV1
+mkdir runs
+cd ../../fine_tuned_models/
+mkdir wav2vec2_NO_customLossV1
+cd ../github/fine-tuning-wav2vec2-NO/
+
+python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV1"\
+                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV1/"\
+                                           --num_train_epochs=3\
+                                           --learning_rate=1e-4\
+                                           --use_asd_metric=1
--- test_models.sh
+++ test_models.sh
@@ -15,6 +15,11 @@
 #                         --extract_transcriptions=0
 
 python test_wav2vec2_scoreperutt.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                    --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_customLossV3"\
-                                    --log_file_name="test_log_wav2vec2_NO_customLossV3"\
+                                    --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_origLossV1"\
+                                    --log_file_name="test_log_wav2vec2_NO_origLossV1"\
+                                    --get_orig_model_results=0\
+
+python test_wav2vec2_scoreperutt.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+                                    --fine_tuned_model="../../fine_tuned_models/wav2vec2_NO_customLossV1"\
+                                    --log_file_name="test_log_wav2vec2_NO_customLossV1"\
                                     --get_orig_model_results=0\
