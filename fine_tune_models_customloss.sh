553b0b85fe4d5f3a177827c7b44367509c317941
Janine <janine.rugayan@ntnu.no>
2023-03-20 17:43:03 +0100

    corrected first finetuning

--- fine_tune_models_customloss.sh
+++ fine_tune_models_customloss.sh
@@ -6,7 +6,7 @@ cd ../../fine_tuned_models/
 mkdir wav2vec2_NO_origLossV1
 cd ../github/fine-tuning-wav2vec2-NO/
 
-python3 fine_tuning_wav2vec2_customLoss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
                                            --fine_tuned_model_ver="fine-tuning_wav2vec2_origLossV1"\
                                            --export_model_dir="../../fine_tuned_models/wav2vec2_NO_origLossV1/"\
                                            --num_train_epochs=3\
@@ -15,17 +15,17 @@ python3 fine_tuning_wav2vec2_customLoss.py --original_model="NbAiLab/nb-wav2vec2
 
 
 
-# cd ../../model_ckpts/
-# mkdir fine-tuning_wav2vec2_customLossV1
-# cd fine-tuning_wav2vec2_customLossV1
-# mkdir runs
-# cd ../../fine_tuned_models/
-# mkdir wav2vec2_NO_customLossV1
-# cd ../github/fine-tuning-wav2vec2-NO/
+cd ../../model_ckpts/
+mkdir fine-tuning_wav2vec2_customLossV1
+cd fine-tuning_wav2vec2_customLossV1
+mkdir runs
+cd ../../fine_tuned_models/
+mkdir wav2vec2_NO_customLossV1
+cd ../github/fine-tuning-wav2vec2-NO/
 
-# python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-#                                            --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV1"\
-#                                            --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV1/"\
-#                                            --num_train_epochs=3\
-#                                            --learning_rate=1e-4\
-#                                            --use_asd_metric=1
+python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
+                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV1"\
+                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV1/"\
+                                           --num_train_epochs=3\
+                                           --learning_rate=1e-4\
+                                           --use_asd_metric=1
