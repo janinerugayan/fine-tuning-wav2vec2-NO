bb56a2e6d3677d5987af8532022a924c212afd73
janinerugayan <janine.rugayan@gmail.com>
2024-04-17 14:38:58 +0000

    move to aulus6

--- fine_tune_models_customloss.sh
+++ fine_tune_models_customloss.sh
@@ -1,38 +1,21 @@
 cd ../../model_ckpts/
-mkdir fine-tuning_wav2vec2_customLossTRIAL16_deepthought
-cd fine-tuning_wav2vec2_customLossTRIAL16_deepthought
+mkdir fine-tuning_wav2vec2_customLossTRIAL21_titan1
+cd fine-tuning_wav2vec2_customLossTRIAL21_titan1
 mkdir runs
 cd ../../fine_tuned_models/
-mkdir wav2vec2_NO_customLossTRIAL16_deepthought
+mkdir wav2vec2_NO_customLossTRIAL21_titan1
 cd ../github/fine-tuning-wav2vec2-NO/
 
 python3 fine_tuning_wav2vec2_customlossV3.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossTRIAL16_deepthought"\
-                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossTRIAL16_deepthought/"\
-                                           --num_train_epochs=1\
+                                           --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossTRIAL21_titan1"\
+                                           --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossTRIAL21_titan1/"\
+                                           --num_train_epochs=3\
                                            --learning_rate=1e-4\
                                            --lambda_asd=0.5\
                                            --use_asd_metric=1\
-                                           --wandb_name="TRIAL16_customLoss_allDataSmall_300m_1ep_batched_deepthought"\
-                                           --export_log="./loss_logs/TRIAL16_deepthought_customLoss.txt"
+                                           --wandb_name="TRIAL21_customLoss_allDataSmall_300m_3ep_batched_titan1"\
+                                           --export_log="./loss_logs/customLossTRIAL21_titan1.txt"
 
-# cd ../../model_ckpts/
-# mkdir fine-tuning_wav2vec2_origLossTRIAL5
-# cd fine-tuning_wav2vec2_origLossTRIAL5
-# mkdir run[srese
-# cd ../../fine_tuned_models/
-# mkdir wav2vec2_NO_origLossTRIAL5
-# cd ../github/fine-tuning-wav2vec2-NO/
-
-# python3 fine_tuning_wav2vec2_customlossV3.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-#                                            --fine_tuned_model_ver="fine-tuning_wav2vec2_origLossTRIAL5"\
-#                                            --export_model_dir="../../fine_tuned_models/wav2vec2_NO_origLossTRIAL5/"\
-#                                            --num_train_epochs=1\
-#                                            --learning_rate=1e-4\
-#                                            --lambda_asd=0.1\
-#                                            --use_asd_metric=1\
-#                                            --wandb_name="TRIAL5_origLoss_allDataSmall_300m_3ep"\
-#                                            --export_log="./loss_logs/TRIAL5_origLoss.txt"
 
 # for n in {1..5}
 # do
@@ -40,22 +23,22 @@ python3 fine_tuning_wav2vec2_customlossV3.py --original_model="NbAiLab/nb-wav2ve
 #     echo FINE-TUNING MODEL CUSTOM LOSS w/ LAMBDA 0.$n
 
 #     cd ../../model_ckpts/
-#     mkdir fine-tuning_wav2vec2_customLossV12_lambda0p${n}
-#     cd fine-tuning_wav2vec2_customLossV12_lambda0p${n}
+#     mkdir fine-tuning_wav2vec2_customLossTRIAL21_titan1_lambda0p${n}
+#     cd fine-tuning_wav2vec2_customLossTRIAL21_titan1_lambda0p${n}
 #     mkdir runs
 #     cd ../../fine_tuned_models/
-#     mkdir wav2vec2_NO_customLossV12_lambda0p${n}
+#     mkdir wav2vec2_NO_customLossTRIAL21_titan1_lambda0p${n}
 #     cd ../github/fine-tuning-wav2vec2-NO/
 
 #     python3 fine_tuning_wav2vec2_customloss.py --original_model="NbAiLab/nb-wav2vec2-300m-bokmaal"\
-#                                             --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossV12_lambda0p${n}"\
-#                                             --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossV12_lambda0p${n}/"\
+#                                             --fine_tuned_model_ver="fine-tuning_wav2vec2_customLossTRIAL21_titan1_lambda0p${n}"\
+#                                             --export_model_dir="../../fine_tuned_models/wav2vec2_NO_customLossTRIAL21_titan1_lambda0p${n}/"\
 #                                             --num_train_epochs=3\
 #                                             --learning_rate=1e-4\
 #                                             --lambda_asd=0.${n}\
 #                                             --use_asd_metric=1\
-#                                             --wandb_name="V12_lambda0p${n}_customLoss_allDataSmall_300m_3ep"\
-#                                             --export_log="./loss_logs/V12_lambda0p${n}_customLoss.txt"
+#                                             --wandb_name="TRIAL21_lambda0p${n}_customLoss_allDataSmall_300m_3ep_batched_titan1"\
+#                                             --export_log="./loss_logs/customLossTRIAL21_titan1_lambda0p${n}.txt"
 
 # done
 
